533dcfc8229d21c9d2456d9edd8ebdf2e6aeea88
Saifs16 <saifsultan2013@gmail.com>
2023-06-21 14:02:08 +0400

    Evaulation and Saving the model

--- code.py
+++ code.py
@@ -14,7 +14,7 @@ from tensorflow.keras.layers import Dense,Conv2D,MaxPool2D,Flatten,Dropout, Batc
 from tensorflow.keras.callbacks import EarlyStopping
 
 # Define the general path
-path = '../Attempt2/archive/'
+path = '../archive/'
 
 # Use ImageDataGenerator to rescale the images
 train_datagen = ImageDataGenerator(rescale=1./255)
@@ -44,3 +44,40 @@ test_generator = test_datagen.flow_from_directory(directory=path +'test',
                                                   class_mode=None,
                                                   shuffle=False,
                                                   seed=42)
+
+model= Sequential()
+
+model.add(Conv2D(filters=128,strides= 1, kernel_size = (5,5), activation='relu', input_shape=(224,224,1,)))
+model.add(MaxPool2D(3,3))
+model.add(Conv2D(filters=64,kernel_size = (5,5),activation='relu'))
+model.add(MaxPool2D(3,3))
+
+model.add(Conv2D(filters=30,kernel_size = (3,3),activation='relu'))
+model.add(MaxPool2D(2,2))
+model.add(Conv2D(filters=30,kernel_size = (3,3),activation='relu'))
+model.add(MaxPool2D(2,2))
+
+model.add(Flatten())
+model.add(Dense(2048,activation='relu'))
+model.add(Dense(512,activation='relu'))
+model.add(Dropout(.1))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(.1))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(4,activation = 'softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer ='adam', metrics= ['accuracy'])
+model.summary()
+
+
+# Hyperparameters
+STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size
+STEP_SIZE_VALID = valid_generator.n//valid_generator.batch_size
+# Stop the training when there is no improvement after 3 epochs trainings.
+early_stop = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+# Train the model
+model.fit(train_generator,steps_per_epoch=STEP_SIZE_TRAIN,
+          validation_data=valid_generator,verbose= 1,
+          validation_steps=STEP_SIZE_VALID,
+          epochs=15, callbacks=early_stop)
